fb57f1038fc67e1984ee936fb4ff41b4bd628e7a
hophacker <j@paiyou.co>
2020-09-13 15:30:35 +0800

    first blood

---
+++ gatsby-node.js
@@ -0,0 +1,35 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+
+exports.createPages = ({ actions: { createPage } }) => {
+  createPage({
+    path: "/no-data/",
+    component: require.resolve("./src/templates/no-data.js"),
+  })
+  createPage({
+    path: "/page-with-context/",
+    component: require.resolve("./src/templates/with-context.js"),
+    context: {
+      title: "We Don’t Need No Stinkin’ GraphQL!",
+      content: "<p>This is page content.</p><p>No GraphQL required!</p>",
+    },
+  })
+  const products = require("./data/products.json")
+  products.forEach(product => {
+    createPage({
+      path: `/product/${product.slug}/`,
+      component: require.resolve("./src/templates/product.js"),
+      context: {
+        title: product.title,
+        description: product.description,
+        image: product.image,
+        price: product.price,
+      },
+    })
+  })
+}
